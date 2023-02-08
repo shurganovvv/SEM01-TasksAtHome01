903f9f1304d17ce27941fa8d1cbf1a7c2a298267
viktor <shurganovvv@yandex.ru>
2023-02-08 18:19:01 +0300

    end Task02-ComparingTwoNumbers project

--- Task02-ComparingTwoNumbers/Program.cs
+++ Task02-ComparingTwoNumbers/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿// Задача 2: Напишите программу, которая 
+//     на вход принимает два числа и Артём 
+//     выдает, какое число большее, а какое меньшее.
+//         a = 5; b = 7 ->  max = 7
+//         a = 2 b = 10 -> max = 10
+//         a = -9 b = -3 -> max = -3
+
 Console.WriteLine("Hello, World!");
+Console.WriteLine("Будем надеяться, что я смогу справиться со сравнением двух чисел (:");
+
+Console.Write("Первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+
+if (number1 > number2)
+{
+    Console.WriteLine($"Полагаю, что {number1} больше чем {number2}");
+}
+else if (number1 < number2)
+{
+    Console.WriteLine($"min = {number1}, max = {number2}");
+}
+else
+{
+    Console.WriteLine("Блин! вот это ты задал задачку...");
+    Console.WriteLine("Что-то мне подсказывает...");
+    Console.WriteLine("что... они... равны...");
+    Console.WriteLine(" ... ");
+    Console.WriteLine("кажется...");
+}
