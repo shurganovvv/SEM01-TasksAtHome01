439a8cb29b0b1ab0e93a5d6c02d0f738d556a0cd
viktor <shurganovvv@yandex.ru>
2023-02-08 19:45:03 +0300

    add Task04-max_u3_TPEX project

---
+++ Task04-max_u3_TPEX/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая 
+//     принимает на вход три числа 
+//     и выдает максимальное из этих чисел.
+//         2, 3, 7 -> 7
+//         44 5 78 -> 78
+//         22 3 9 -> 22
+
+Console.WriteLine("Прив#т, Артём!.. так, это я что-то отвлекаюсь...");
+Console.WriteLine("Нам же нужно определить максимальное из трёх чисел (:");
+
+Console.Write("Первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("и, наконец, Третье число: ");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+if (max < number2)
+{
+    max = number2;
+}
+if (max < number3)
+{
+    max = number3;
+}
+Console.WriteLine("max = " + max);
